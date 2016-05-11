72be3a671874e8cfb67dacddbe5957c3c91217a3
supersveto <haritonov@protonmail.com>
2016-05-11 12:26:05 +0200

    Added challenge description

--- 04-Variables-Datatypes/CharAndBoolean/src/com/sh/Main.java
+++ 04-Variables-Datatypes/CharAndBoolean/src/com/sh/Main.java
@@ -10,6 +10,10 @@ public class Main {
         boolean myBoolean = false;
         boolean isMale = true;
 
+        // 1. Find the code for the registered symbol on the same line as the copyright symbol.
+        // 2. Create a variable of type char and assign it the unicode value for that symbol.
+        // 3. Display in on screen.
+
         char registeredSymbol = '\u00AE';
         System.out.println("Registered symbol: " + registeredSymbol);
     }
