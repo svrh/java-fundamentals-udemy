57a97870144124f8e705bf331d52a0a69165164e
supersveto <haritonov@protonmail.com>
2016-05-11 12:24:29 +0200

    Added challenge description

--- 04-Variables-Datatypes/ByteShortInt/src/com/sh/Main.java
+++ 04-Variables-Datatypes/ByteShortInt/src/com/sh/Main.java
@@ -19,5 +19,17 @@ public class Main {
         // long has a width of 64
         long longValue = 9_223_372_036_854_775_807L;
 
+        // 1. Create a byte variable and set it to any valid byte number.
+        // 2. Create a short variable and set it to any valid short number.
+        // 3. Create a int variable and set it to any valid in number.
+        // 4. Create a variable of type long, and make it equal to
+        //    50000 + 10 times the sum of the byte, plus the short plus the int
+
+        byte byteVar = 127;
+        short shortVar = 32000;
+        int intVar = 1000000;
+        long longVar = 50000L + 10L * (byteVar + shortVar + intVar);
+        System.out.println("longVar: " + longVar);
+
     }
 }
